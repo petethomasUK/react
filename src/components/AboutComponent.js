9ad60677722d10da6a485c958917b4e0ddd987b8
Peter Thomas <pete@MacBook-Pro.local>
2019-11-29 16:17:42 +0000

    week2

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -6,7 +6,18 @@ function About(props) {
 
     const RenderLeader = ({leader}) => {
       return (
-        <p>Leader {leader.name}</p>
+        <div key={leader.id} className="col-12 mt-5">
+        <Media tag="li">
+          <Media left middle>
+              <Media object src={leader.image} alt={leader.name} />
+          </Media>
+          <Media body className="ml-5">
+            <Media heading>{leader.name}</Media>
+            <p>{leader.designation}</p>
+            <p>{leader.description}</p>
+          </Media>
+        </Media>
+      </div>
       )
     }
 
